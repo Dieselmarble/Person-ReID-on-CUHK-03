091ae5ca81fbb380b4974aebb1221a2f6f0fb5ba
Dieselmarble <lizhongxuanchina@gmail.com>
2018-12-04 21:00:17 +0000

    set split

--- temp.py
+++ temp.py
@@ -15,27 +15,38 @@ import time
 import sys
 import json
 
-# use 100 randomly selected identities from training set as validation set
 # 1467 identities in total
 num_identies = 1467
 num_validation = 100  
+rnd = np.random.RandomState(3)
 
-camId = loadmat('PR_data/cuhk03_new_protocol_config_labeled.mat')['camId'].flatten()
-filelist = loadmat('PR_data/cuhk03_new_protocol_config_labeled.mat')['filelist'].flatten()
-labels = loadmat('PR_data/cuhk03_new_protocol_config_labeled.mat')['labels'].flatten()
-train_idx = loadmat('PR_data/cuhk03_new_protocol_config_labeled.mat')['train_idx'].flatten()
-#only for testing the design
-gallery_idx = loadmat('PR_data/cuhk03_new_protocol_config_labeled.mat')['gallery_idx'].flatten()
-query_idx = loadmat('PR_data/cuhk03_new_protocol_config_labeled.mat')['query_idx'].flatten()
-with open('PR_data/feature_data.json', 'r') as f:
-    features = json.load(f)
-features = np.asarray(features) 
+#camId = loadmat('PR_data/cuhk03_new_protocol_config_labeled.mat')['camId'].flatten()
+#filelist = loadmat('PR_data/cuhk03_new_protocol_config_labeled.mat')['filelist'].flatten()
+#labels = loadmat('PR_data/cuhk03_new_protocol_config_labeled.mat')['labels'].flatten()
+#train_idx = loadmat('PR_data/cuhk03_new_protocol_config_labeled.mat')['train_idx'].flatten()
+##only for testing the design
+#gallery_idx = loadmat('PR_data/cuhk03_new_protocol_config_labeled.mat')['gallery_idx'].flatten()
+#query_idx = loadmat('PR_data/cuhk03_new_protocol_config_labeled.mat')['query_idx'].flatten()
+#with open('PR_data/feature_data.json', 'r') as f:
+#    features = json.load(f)
+#features = np.asarray(features) 
 
 features_train = features[train_idx-1,:]
 # find distinct identities in training set (should be 767 refer to the protocol)
+train_label = labels[train_idx-1,]
 iden_train = np.unique(labels[train_idx-1,])
-valid_label = np.random.choice(iden_train, num_validation)
+#train_set = np.column_stack((train_idx,labels[train_idx-1,].T))
 
+# use 100 randomly selected identities from training set as validation set
+valid_label = rnd.choice(iden_train, num_validation,replace=False)
+
+valid_index = []
+for i in range (num_validation):
+    valid_index.append(np.argwhere(train_label == valid_label[i]))
+
+valid_index = np.concatenate(valid_index, axis=0)
+valid_idx = train_idx[valid_index].ravel()
+train_idx_new = np.delete(train_idx, valid_index)
 
 # =============================================================================
 # if __name__ == "__main__":
